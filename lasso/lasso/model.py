56e383979ce4fb8daa5f4e364ac65725e0fc97a6
LeoM <meillier.leo@gmail.com>
2025-09-22 17:41:52 +0200

    modif struct

--- lasso/lasso/model.py
+++ lasso/lasso/model.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-
 from plot import plot_coefficient_paths, plot_mse_vs_alpha
 
 
